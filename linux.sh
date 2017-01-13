59cb4635d51b504c4d1ee1eb567f90512915f743
maijou2501 <maijou2501@gmail.com>
2017-01-14 00:28:53 +0900

    [clean] execute shellcheck

--- linux.sh
+++ linux.sh
@@ -11,11 +11,11 @@ else
 
 	source ~/.bashrc
 
-	ln -s $1/.inputrc ~/.inputrc
-	ln -s $1/.ctags   ~/.ctags
-	ln -s $1/.toprc   ~/.toprc
-	mkdir $XDG_CONFIG_HOME/nvim
-	ln -s $1/init.vim $XDG_CONFIG_HOME/nvim/init.vim
-	ln -s $1/dein.toml $XDG_CONFIG_HOME/nvim/dein.toml
-	ln -s $1/dein_lazy.toml $XDG_CONFIG_HOME/nvim/dein_lazy.toml
+	ln -s "$1"/.inputrc ~/.inputrc
+	ln -s "$1"/.ctags   ~/.ctags
+	ln -s "$1"/.toprc   ~/.toprc
+	mkdir "$XDG_CONFIG_HOME"/nvim
+	ln -s "$1"/init.vim "$XDG_CONFIG_HOME"/nvim/init.vim
+	ln -s "$1"/dein.toml "$XDG_CONFIG_HOME"/nvim/dein.toml
+	ln -s "$1"/dein_lazy.toml "$XDG_CONFIG_HOME"/nvim/dein_lazy.toml
 fi
