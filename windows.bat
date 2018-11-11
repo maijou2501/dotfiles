de5ea416c936704b6f1ef4dcacb095b9c2be5948
maijou2501 <maijou2501@gmail.com>
2018-11-11 23:09:31 +0900

    [clean] delete unnecessary lines and files

--- windows.bat
+++ windows.bat
@@ -1,5 +1,5 @@
 chdir %HOMEDRIVE%%HOMEPATH%\.dotfiles
-mklink %HOMEDRIVE%%HOMEPATH%"\_vimrc" %HOMEDRIVE%%HOMEPATH%"\.dotfiles\_vimrc"
-mklink %HOMEDRIVE%%HOMEPATH%"\_gvimrc" %HOMEDRIVE%%HOMEPATH%"\.dotfiles\_gvimrc"
+REM mklink %HOMEDRIVE%%HOMEPATH%"\_vimrc" %HOMEDRIVE%%HOMEPATH%"\.dotfiles\_vimrc"
+REM mklink %HOMEDRIVE%%HOMEPATH%"\_gvimrc" %HOMEDRIVE%%HOMEPATH%"\.dotfiles\_gvimrc"
 mklink %HOMEDRIVE%%HOMEPATH%"\Documents\AutoHotkey.ahk" %HOMEDRIVE%%HOMEPATH%"\.dotfiles\AutoHotkey.ahk"
 exit 0
